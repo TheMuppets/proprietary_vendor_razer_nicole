744f93ea42fc156415a5ff6d2876f91ede09da96
AnierinB <anierin@evolution-x.org>
2025-12-18 18:13:54 +0000

    nicole: Import blobs & firmware from SKQ1.211103.001
    * Kang IMS from otter as ours crashes on IMS Factory HAL init.
    * Kang Euicc from otter to keep it simple.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ Android.mk
@@ -0,0 +1,28 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),nicole)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,9065c71b2ddc214acf2c091241f28bd366b41eae)
+$(call add-radio-file-sha1-checked,radio/aop.img,20fce0b8ccdbab328f525d7f2243b950fc0e2e52)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,5b6c6bda41dd0b503cfa949696371ee5bb4c0b47)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,06d9762296431f0641a4f0b236d51dad3a0104e5)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,0f6bb5317adffcb3cb9691ad4c83d447736d0d0f)
+$(call add-radio-file-sha1-checked,radio/dsp.img,2b745553be156514fb0a95d41a48b39ead83c29d)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,24213e04fff7b9205cf35efa41a18571d5818c31)
+$(call add-radio-file-sha1-checked,radio/hyp.img,f52f44fcf91a20a7d6a5553b37e115656aea45b8)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,a2361fbe46472361ce21b578150deae0fc3f1d6c)
+$(call add-radio-file-sha1-checked,radio/modem.img,c120a38b7b982fa174a7e8c6d5c8d529eb79512b)
+$(call add-radio-file-sha1-checked,radio/multiimgoem.img,8ff8a90a1dc511b3747ba18b6d1facaa6512d89e)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,e7a757d9dabe36a6986e93f3baea468564c9de75)
+$(call add-radio-file-sha1-checked,radio/shrm.img,aca4de9763e4a07bd4116834fab003121510d6b3)
+$(call add-radio-file-sha1-checked,radio/tz.img,6daf1aad95a7751f623113046908c730e3e1567d)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,7f4d4ec50a85798627c92fdb559d6db754bcdcfc)
+$(call add-radio-file-sha1-checked,radio/vm-bootsys.img,86ecc1a63e9f33191f3c2e7a4619562466ab569d)
+$(call add-radio-file-sha1-checked,radio/xbl.img,e6ea308883ef16dea86c0aa240fff169415fb05a)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,df67cd6b7a320c9370176b0bc81169b381714cb8)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,23 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    bluetooth \
+    cpucp \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    keymaster \
+    modem \
+    multiimgoem \
+    qupfw \
+    shrm \
+    tz \
+    uefisecapp \
+    vm-bootsys \
+    xbl \
+    xbl_config
